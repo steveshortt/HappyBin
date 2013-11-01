d7f440e0fa12bf4cc1d7f74cb6b0c463edf0efdc
Steve Shortt <17773746+steveshortt@users.noreply.github.com>
2013-11-01 17:54:54 +0000

    Quick fix to InitializePatchStatus; added ico

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HappyBin.Properties {
+namespace HappyBin.AutoUpdater.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
