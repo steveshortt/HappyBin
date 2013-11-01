487bfd4739a99d368de5a6d237cb09aa0fb00974
steveshortt_cp <SND\steveshortt_cp>
2013-11-01 17:39:35 +0000

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace HappyBin.AutoUpdater
+{
+	public partial class App : Application
+	{
+		public Updater Updater { get; private set; }
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup( e );
+
+			this.Updater = new Updater();
+			this.Updater.InitializePatchStatus();
+
+			if( this.Updater.Status.PatchFilePathExists )
+			{
+				MainDlg mainDlg = new MainDlg( this.Updater );
+				mainDlg.Show();
+			}
+			else
+			{
+				this.Updater = null;
+				App.Current.Shutdown();
+			}
+		}
+	}
+}
---
+++ Code/LogMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HappyBin.AutoUpdater
+{
+	public class LogMessage
+	{
+		string _message = null;
+
+		public LogMessage() { }
+		public LogMessage(string message)
+		{
+			this.Message = message;
+		}
+		public LogMessage(string format, params object[] args)
+		{
+			this.Message = string.Format( format, args );
+		}
+
+		public string Message
+		{
+			get { return _message; }
+			set
+			{
+				if( _message != value )
+				{
+					_message = value;
+					this.TimeStamp = DateTime.Now;
+				}
+			}
+		}
+
+		public DateTime TimeStamp { get; private set; }
+	}
+}
